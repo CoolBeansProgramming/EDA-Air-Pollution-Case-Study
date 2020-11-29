4c990cc2d32312581fa9238c56b8b86ae6794ead
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-11-29 17:15:24 -0500

    Plot 2 files

---
+++ plot2.R
@@ -0,0 +1,18 @@
+# download file and unzip file 
+fileURL <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileURL, destfile="air.zip", method="curl")
+
+unzip(zipfile="air.zip")
+
+NEI <-readRDS("summarySCC_PM25.rds")
+SCC <-readRDS("Source_Classification_Code.rds")
+
+# subset emissions data for Balitmore only
+baltimore <-NEI[NEI$fips=="24510", ]
+aggSumYearBalt <-aggregate(Emissions~year, baltimore, sum)
+
+# create and save boxplot 
+png('plot2.png')
+barplot(height = aggSumYearBalt$Emissions, names.arg = aggSumYearBalt$year, xlab="Year", ylab="Total Emissions", main="Total PM2.5 emissions in Baltimore City, MD from 1999-2008")
+dev.off()
+
