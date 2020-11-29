0e90c0e609407fca7391750c332b23f873ec1ead
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-11-29 17:04:43 -0500

    Plot 1 files

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# download file and unzip file 
+fileURL <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileURL, destfile="air.zip", method="curl")
+
+unzip(zipfile="air.zip")
+
+NEI <-readRDS("summarySCC_PM25.rds")
+SCC <-readRDS("Source_Classification_Code.rds")
+
+# calculate total emission for each year
+aggregatedSumYear <- aggregate(Emissions~year, data=NEI, FUN=sum)
+
+# create and save barplot 
+png('plot1.png')
+barplot(height = aggregatedSumYear$Emissions, names.arg=aggregatedSumYear$year, xlab="Year", ylab="Total Emissions", main="Total PM2.5 emissions from 1999-2008")
+dev.off()
